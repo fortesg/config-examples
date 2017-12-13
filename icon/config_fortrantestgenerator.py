201f6896262a8de021fd6921b181fc194ec49c8d
Christian Hovy <hovy@informatik.uni-hamburg.de>
2017-12-13 13:37:49 +0100

    [icon] Added ICON example

---
+++ icon/config_fortrantestgenerator.py
@@ -0,0 +1,12 @@
+import os
+
+FTG_DIR = os.path.dirname(os.path.realpath(__file__))
+
+FCG_DIR = FTG_DIR + '/../fortrancallgraph'
+FCG_CONFIG_FILE = 'config_fortrancallgraph.py'
+
+ICON_DIR = '/home/christian/workspace/icon'
+
+TEMPLATE_DIR = FTG_DIR + '/templates/icon_standalone'
+TEST_SOURCE_DIR = ICON_DIR + '/src/tests'
+TEST_DATA_BASE_DIR = ICON_DIR + '/ftg'
